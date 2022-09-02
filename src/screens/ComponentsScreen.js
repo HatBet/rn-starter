f3b417670e02b9985b3dff2e289e750d99b8112d
betul <betul.aydemir@mahrek.com.tr>
2022-09-02 15:46:45 +0300

    first commit

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native-web";
+
+const ComponentsScreen = () => {
+    return <Text style={{fontSize: 30}}> This is the components</Text>
+};
+
+// const styles = StyleSheet.create({
+//     textStyle: {
+//         fontSize:30
+//     }
+// });
+
+export default ComponentsScreen;
